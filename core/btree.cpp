618eb09bf9e42d60880d19e0eb5bad6f627f90c1
Erik Langert <eriklangert@gmail.com>
2015-05-01 14:04:02 -0400

    adding little things

--- core/btree.cpp
+++ core/btree.cpp
@@ -11,14 +11,19 @@ btree<T>::btree(int d) {
 template<typename T>
 btree<T>::~btree() {
 	// I dont think this will do it.
-	// The root will be gone, but all the other nodes will still me in memory
+	// The root will be gone, but all he other nodes will still me in memory
 	// I think we might have to do something like cleanBst();
     delete root; //temp
 }
 
 template <typename T>
 void btree<T>::insert(T value) {
-    insert(value, root);
+	if (root == nullptr) {
+		node<T>* newNode = new node<T>(value);
+		root = newNode;
+	} else {
+		insert(value, root);
+	}
 }
 
 /*
@@ -26,12 +31,18 @@ template <typename T>
 void btree<T>::insert(T value, node<T> *nd) {
     
 }
+*/
 
 template <typename T>
 node<T>* btree<T>::search(T value) {
-
+	if (root == nullptr) {
+		return nullptr;
+	} else {
+		return search(value, root);
+	}
 }
 
+/*
 template <typename T>
 node<T>* btree<T>::search(T value, node<T> *nd) {
 
