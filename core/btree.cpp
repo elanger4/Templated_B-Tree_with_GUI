e79300a1b50262f10e6769f9043d931a7c0d165b
Erik Langert <eriklangert@gmail.com>
2015-05-02 01:08:12 -0400

    uncommented a whole bunch. Fixed alot of the little errors

--- core/btree.cpp
+++ core/btree.cpp
@@ -10,11 +10,7 @@ btree<T>::btree(int d) {
 
 template<typename T>
 btree<T>::~btree() {
-    // I dont think this will do it.
-    // The root will be gone, but all he other nodes will still me in memory
-    // I think we might have to do something like cleanBst();
-//		btreeClean(root);
-//    delete root; //temp
+		btreeClean(root);
 }
 
 /*
@@ -28,83 +24,78 @@ void btree<T>::insert(T value) {
     }
 } */
 
-template <typename T>
-void btree<T>::insert(T value) {  // k=value
-    node<T> *x = root;
-    while (!x->leaf) {
-        int i = 0;
-        while (value > x->keys[i] && i < x->keys.length())
-            i++;
-        node<T> *y = x->children[i];
-        if (y->keys.length() < degree) //potential off by one
-            x = y;
-        else {
-            //split y and change x to point to one of the two parts of y.
-            //If k is smaller than mid key in y, then set x as first part of y.
-                //Else second part of y. 
-            //When we split y, we move a key from y to its parent x.
-        }
-    }
+// template <typename T>
+// void btree<T>::insert(T value) {  // k=value
+	
+    // node<T> *x = root;
+    // while (!x->leaf) {
+        // int i = 0;
+        // while (value > x->keys[i] && i < x->keys.length())
+            // i++;
+        // node<T> *y = x->children[i];
+        // if (y->keys.length() < degree) //potential off by one
+            // x = y;
+        // else {
+            // //split y and change x to point to one of the two parts of y.
+            // //If k is smaller than mid key in y, then set x as first part of y.
+                // //Else second part of y. 
+            // //When we split y, we move a key from y to its parent x.
+        // }
+    //}
     //x is now a leaf. 
     //insert k into x basic
+	
 
-}
+//}
 
 
 template <typename T>
 node<T>* btree<T>::search(T value) {
 
-    /*
     if (root == nullptr) {
         return nullptr;
     } else {
         return search(value, root);
     }
-    */
 }
 
 
-/*
+
    template <typename T>
    node<T>* btree<T>::search(T value, node<T> *nd) {
-		for (int i = 0; i < nd.keys.size(); i++) {
-			if (value === nd.keys[i]) {
+		for (unsigned long i = 0; i < nd->keys.size(); i++) {
+			if (value == nd->keys[i]) {
 				return nd;
-			} else if (value < nd.keys[i]) {
-				search(value, nd.keys[i]);
+			} else if (value < nd->keys[i]) {
+				search(value, nd->keys[i]);
 				break;
-			} else if (val > nd.keys.size()-1) {
-				search(nd.children[nd.children.size()-1], value)
+			} else if (value > nd->keys.size()-1) {
+				search(nd->children[nd->children.size()-1], value);
 				break;
 			}
 		}
    }
 
-   template <typename T>
-   void btree<T>::printInOrder() {
-		if (root == nullptr)
-			return;
 
-		inOrder(root);
-   }
+template <typename T>
+void btree<T>::printInOrder() {
+	if (root == nullptr)
+		return;
 
-   template <typename T>
-   void btree<T>::printInOrder(node<T> *nd) {
-		for (int i = 0; i < nd.children.size(); i++) {
-			if (nd.children[i] != nullptr) {
-				inOrder(nd.children[i];
-			} else {
-				if (i < nd.keys.size()) {
-					cout << nd.keys[i];
-				}
-			}
-		}
-   }
-   */
+	inOrder(root);
+}
 
 template <typename T>
 void btree<T>::printInOrder(node<T> *nd) {
-
+	for (unsigned long i = 0; i < nd->children.size(); i++) {
+		if (nd->children[i] != nullptr) {
+			inOrder(nd->children[i]);
+		} else {
+			if (i < nd->keys.size()) {
+				std::cout << nd->keys[i];
+			}
+		}
+	}
 }
 
 template <typename T>
@@ -122,6 +113,7 @@ void btree<T>::writeFile() {
     
 }
 
+template <typename T>
 void btree<T>::btreeClean() {
 	if (root == nullptr) {
 		return;
@@ -130,13 +122,13 @@ void btree<T>::btreeClean() {
 	root = nullptr;
 }
 
+template <typename T>
 void btree<T>::btreeClean(node<T> *nd) {
 	if (nd == nullptr)
 		return;
-	for (int i = 0; i < nd.children.size(); i++) {
-		btreeClean(nd.children[i];
+	for (unsigned long i = 0; i < nd->children.size(); i++) {
+		btreeClean(nd->children[i]);
 	}
 	 delete nd;
 }
 
-*/
