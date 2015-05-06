b5e00c51c720120afdbf032f4e9523b696f571e0
Erik Langert <eriklangert@gmail.com>
2015-05-06 15:02:22 -0400

    Life is meaningless.  Everything ends

--- core/main.cpp
+++ core/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    btree<int> b_tree(5);
+    btree<double> b_tree(5);
 	b_tree.insert(1);
 	
 
--- core/node.h
+++ core/node.h
@@ -3,22 +3,27 @@
 
 #include <iostream>
 #include <vector>
+
 template <typename T>
 class node {
     public:
         node(){};
         ~node(){};
-        int leafInsert(T value) {
+
+        int nodeInsert(T value) {
             int i = 0;
-            while (value > keys[i])
-                i++;
+            while (value > keys[i]) {
+				i++;
+			}
+
             keys.insert(i, value);
 			return i;
         }
+		
         std::vector< node<T>* > children;
-        std::vector< T* > keys;
+        std::vector<T> keys;
 		node<T> *parent;
-        bool leaf;
+        bool isLeaf;
 
 };
 
